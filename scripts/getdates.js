0a931c4890741c030ef2e68fc4eb8cf20754da71
Angelo Antimo <angeloantimo14@gmail.com>
2026-09-06 02:23:28 +0800

    update

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastmodified").textContent = document.lastModified;
