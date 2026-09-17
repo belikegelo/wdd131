d696d4edc9a5adb981846029e9ad5a97674e25b3
Angelo Antimo <angeloantimo14@gmail.com>
2026-09-17 11:16:28 +0800

    update

--- scripts/place.js
+++ scripts/place.js
@@ -1,22 +1,24 @@
-const temperature = 8;
-const windSpeed = 12;
-
+const temperature = 31;
+const windSpeed = 5;
 
 // Calculate wind chill in Celsius and km/h
 function calculateWindChill(temperature, windSpeed) {
-    return 13.12 + (0.6215 * temperature) - (11.37 * windSpeed ** 0.16) + (0.3965 * temperature * windSpeed ** 0.16);
+    return 13.12
+        + (0.6215 * temperature)
+        - (11.37 * windSpeed ** 0.16)
+        + (0.3965 * temperature * windSpeed ** 0.16);
 }
 
+// Display temperature and wind speed
+document.querySelector("#temperature").textContent = `${temperature} °C`;
+document.querySelector("#wind-speed").textContent = `${windSpeed} km/h`;
 
 // Check if wind chill calculation is possible
 if (temperature <= 10 && windSpeed > 4.8) {
-
     const windChill = calculateWindChill(temperature, windSpeed);
 
     document.querySelector("#wind-chill").textContent =
         `${windChill.toFixed(1)} °C`;
-
 } else {
-
     document.querySelector("#wind-chill").textContent = "N/A";
 }
